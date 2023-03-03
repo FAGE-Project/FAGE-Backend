aa3a9f0132cc36ac7554b59767a3380be50494a6
unknown <allan-sjp@hotmail.com>
2023-03-02 22:20:13 -0300

    Configuração inicial

---
+++ src/main/java/com/example/fage/FageApplication.java
@@ -0,0 +1,13 @@
+package com.example.fage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fage/FageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

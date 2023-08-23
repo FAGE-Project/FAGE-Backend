ccfe01f241f7516a7f91e1b58149ab0852e15f69
Allan Fernando <allan-sjp@hotmail.com>
2023-08-22 22:30:42 -0300

    EndPoint de empresas

--- src/main/java/com/example/fage/controller/EmpresaController.java
+++ src/main/java/com/example/fage/controller/EmpresaController.java
@@ -1,8 +1,12 @@
 package com.example.fage.controller;
 
 import com.example.fage.dto.LoginDto;
+import com.example.fage.entity.Empresa;
 import com.example.fage.dto.EmpresaDto;
 import com.example.fage.service.EmpresaService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,4 +32,21 @@ public class EmpresaController {
                     .body(e.getMessage());
         }
     }
+
+    @GetMapping
+    public  ResponseEntity<?> buscaTodos(){
+        try {
+             List<Empresa> empresas = empresaService.listarTodos();
+            return ResponseEntity.status(HttpStatus.OK).body(empresas);
+        } catch (Exception e) {
+           return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
+    }
+    
+    
+    // @GetMapping("/nome")
+    // public ResponseEntity<?>  buscaPorNome
+
 }
